91278004bb8e87eddc872748576978cd6efe8b5c
Karan <sayantankaran73@gmail.com>
2022-10-18 12:26:11 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+var audio = document.createElement('audio');
+audio.setAttribute('src','src/sound.mp3');
+audio.loop=true;
+audio.play();
+
+
+
+// var audio = new Audio("./sound.mp3");
+// audio.loop=true;
+
+// audio.play();
+
+
+
+
+// for (var i = 0; i < 3; i++) {
+
+//     document.querySelector(".mybutton")[i].addEventlistener("click", function () {
+//         var test = this.innerHtml
+//         switch (key) {
+//             case a:
+//                 var audio = new Audio("./sound.mp3");
+//                 audio.play();
+//                 break;
+//             case b:
+//                 var audio = new Audio("./sound.mp3");
+//                 audio.play();
+//                 break;
+//             case c:
+//                 var audio = new Audio("./sound.mp3");
+//                 audio.play();
+//                 break;
+//             default:
+//                 break;
+//         }
+//     })
+
+// }
+
